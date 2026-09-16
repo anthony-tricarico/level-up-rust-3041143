6868dcde7c196aac0a2f3834826c05f5c83e56a8
Anthony Tricarico <128626789+anthony-tricarico@users.noreply.github.com>
2026-09-16 07:20:38 +0000

    wrote solution

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
+/* How to find the middle value given the length of a list?
+To do that we can first sort the array.
+Then we should retrieve the index of the middle value by dividing
+by two and truncating to the integer part. */
 fn median(a: Vec<f32>) -> Option<f32> {
-    todo!();
+    let mut b = a.clone();
+    let len = b.len();
+    if b.is_empty() {
+        return None;
+    }
+
+    if !len.is_multiple_of(2) {
+        let middle = len / 2;
+        b.sort_by(|a, b| a.total_cmp(b));
+        Some(b[middle])
+    } else {
+        let middle = len / 2;
+        let before = middle - 1;
+        b.sort_by(|a, b| a.total_cmp(b));
+        let result = (b[middle] + b[before]) / 2.0;
+        Some(result)
+    }
 }
 
 fn main() {
